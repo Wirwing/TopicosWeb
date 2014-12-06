46fb042bc9082883eb278b70271d877d3eb2cc16
Wirwing <irving.cf@gmail.com>
2014-12-06 11:28:23 -0600

    Se agrega servicio de loggeo de autos: añadir y consulta.

---
+++ RentalHubService/RentalHubService/BookService.svc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RentalHubService
+{
+    
+    public class CarBookService : ICarBookService
+    {
+
+        private static List<CarBooking> Bookings = new List<CarBooking>()
+        {
+            new CarBooking() { RentId = "123", Provider = "Proveedor1"},
+            new CarBooking() { RentId = "324", Provider = "Proveedor1"},
+            new CarBooking() { RentId = "456", Provider = "Proveedor1" },
+            new CarBooking() { RentId = "678", Provider = "Proveedor1" }
+        };
+        
+        public List<CarBooking> AddLoggedCarBookings(CarBooking booking)
+        {
+            Bookings.Add(booking);
+            return Bookings;
+        }
+
+        public List<CarBooking> GetLoggedBookings()
+        {
+            return Bookings;
+        }
+
+    }
+}
---
+++ RentalHubService/RentalHubService/ICarBookService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RentalHubService
+{
+    [ServiceContract]
+    public interface ICarBookService
+    {
+
+        [OperationContract]
+        List<CarBooking> AddLoggedCarBookings(CarBooking booking);
+
+        [OperationContract]
+        List<CarBooking> GetLoggedBookings();
+
+    }
+
+    [DataContract]
+    public class CarBooking
+    {
+
+        private string rentId;
+        private string provider;
+
+        [DataMember]
+        public string Provider
+        {
+            get { return provider; }
+            set { provider = value; }
+        }
+
+        [DataMember]
+        public string RentId
+        {
+            get { return rentId; }
+            set { rentId = value; }
+        }
+    }
+}
