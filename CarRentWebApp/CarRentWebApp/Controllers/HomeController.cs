4fb5187fba2098adaa743f73a1e952a8ff23770b
Wirwing <irving.cf@gmail.com>
2014-12-09 12:21:59 -0600

    Se agrega proyecto client web app .NET ASP MVC.

---
+++ CarRentWebApp/CarRentWebApp/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CarRentWebApp.CarServiceReference;
+
+namespace CarRentWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            var client = new GetCarsWSDLPortTypeClient();
+            
+            car1[] bechCars;
+            var kutzCars = client.GetCarsWSDLOperation(out bechCars);
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
